f8b6f59da523792e808466bee2f90eedfcb599d9
ryo2357 <ryosuke.m.ab@gmail.com>
2024-05-09 14:15:50 +0900

    テストのコピー

--- wcr/src/main.rs
+++ wcr/src/main.rs
@@ -1,3 +1,40 @@
+use anyhow::Result;
+use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `wc`
+struct Args {
+    /// Input file(s)
+    #[arg(value_name = "FILE", default_value = "-")]
+    files: Vec<String>,
+
+    /// Show line count
+    #[arg(short, long)]
+    lines: bool,
+
+    /// Show word count
+    #[arg(short, long)]
+    words: bool,
+
+    /// Show byte count
+    #[arg(short('c'), long)]
+    bytes: bool,
+
+    /// Show character count
+    #[arg(short('m'), long, conflicts_with("bytes"))]
+    chars: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
+// --------------------------------------------------
+fn run(mut args: Args) -> Result<()> {
+    Ok(())
 }
